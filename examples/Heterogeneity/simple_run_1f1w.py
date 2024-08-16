cee048d8d2f535cf50944c3a3c809b482dfb1546
Dr. Malena Orduna Alegria <102863775+MalenaOA@users.noreply.github.com>
2024-08-16 11:14:04 -0500

    removing wd paths

--- examples/Heterogeneity/simple_run_1f1w.py
+++ examples/Heterogeneity/simple_run_1f1w.py
@@ -14,17 +14,6 @@ def get_wd():
     return os.path.dirname(os.path.abspath(__file__))
 wd = get_wd()
 
-# ADD FUNCTION TO GET ABSOLUTE PATH AND DEFINED IT!!
-
-# Malena PC ->
-#wd = r"D:\Malena\CHAMP\PyCHAMP\code_20240704\PyCHAMP\examples\Heterogeneity"
-# Malena Laptop ->
-#wd = r"C:\Users\m154o020\CHAMP\PyCHAMP\Summer2024\code_20240705\PyCHAMP\examples\Heterogeneity"
-# Michelle Laptop 
-wd = r"/Users/michellenguyen/Downloads/PyCHAMP/examples/Heterogeneity"
-
-# Set this to True if you want to load from Outputs directory
-#load_from_outputs = False
 # Load from Outputs directory
 load_from_outputs = True
 
